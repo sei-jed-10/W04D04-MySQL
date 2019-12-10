f55f4065fb5c64d11843de894302a822ed9b1737
saadkhan29 <57666391+saadkhan29@users.noreply.github.com>
2019-12-10 23:42:37 +0300

    Update televisions.sql

--- exercises/computers-lab/televisions.sql
+++ exercises/computers-lab/televisions.sql
@@ -1,4 +1,4 @@
--- Open up psql and create a sql_lab database if you haven't already done so. 
+-- Open up mysql and create a sql_lab database if you haven't already done so. 
 -- If you already have a sql_lab database, no need to create it again.
 
 -- Write SQL commands under the prompts below, and run the file to get results.
@@ -18,9 +18,6 @@
 
 -- Select all entries from the tv_models table
 
-
--- HUNGRY FOR MORE? 
--- Look at this afternoon's instructor notes and read on altering tables before attempting below
 
 -- Alter the tv_models, removing the resolution column
 --  and add vertical_resolution and horizontal_resolution columns
